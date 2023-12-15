a76edd633d10e8371b79499538400572d8922a96
KhanhEK2846 <lehoangkhanh2846912002@gmail.com>
2023-12-15 14:00:21 +0700

    404 Not Found

--- Server/app/app.py
+++ Server/app/app.py
@@ -1,10 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET', 'POST'])
 def LogIn():
-    return render_template('TrangChu.html')
+    if request.method == "GET":
+        return render_template('TrangChu.html')
+    if request.method == "POST":
+        return
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
