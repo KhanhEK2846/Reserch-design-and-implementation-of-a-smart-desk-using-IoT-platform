5bfb8e52923c214403fbd22ca8fe7c2c691efa81
KhanhEK2846 <lehoangkhanh2846912002@gmail.com>
2023-12-18 15:52:24 +0700

    Fix issue

---
+++ Server/app/static/js/TrangChu.js
@@ -0,0 +1,6 @@
+// document.getElementById("Submit").addEventListener("click",Submit);
+// function Submit()
+// {
+//     console.log(document.getElementById("Username").value);
+//     console.log(document.getElementById("Password").value);
+// }
